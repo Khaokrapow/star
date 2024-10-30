a0a98c934f5b86fd1ea4c0d1d58c27ca14c3c692
Kasama Lekmad <kasama.l@ku.th>
2024-10-30 09:13:27 +0700

    add data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [pattern, setPattern] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  //กด submit
+  const handleSubmit = () => {
+    const number = parseInt(inputValue);
+    let result = '';
+
+    // เพิ่มดาว
+    for (let i = 1; i <= number; i++) {
+      result += '*'.repeat(i) + '\n';
+    }
+    // ลดดาว
+    for (let i = number - 1; i >= 1; i--) {
+      result += '*'.repeat(i) + '\n';
+    }
+
+    setPattern(result);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Input">
+      <div>
+        <label>Input : </label>
+        <input type="number" value={inputValue} onChange={handleInputChange} />
+        <button onClick={handleSubmit}>Submit</button>
+        <pre>{pattern}</pre>
+      </div>
     </div>
   );
 }
